491ed58445ceaf80f3aa35827254b54008ad39e4
Admins <nguyenthanhbao9a@gmail.com>
2024-10-13 20:02:42 +0700

    Initial commit

---
+++ configs/bank.php
@@ -0,0 +1,22 @@
+<?php
+$config['bank'] = [
+    'vnpay' => [
+        'vnp_TmnCode' => '8U7CJ3O4',
+        'vnp_HashSecret' => 'GXJYAKJISDJLZERSCUXDSPXITRDHTBHH',
+    ],
+    'momo' => [
+        "partner_code" => "MOMOBKUN20180529",
+        "access_key" => "klm05TvNBzhg7h7j",
+        "secret_key" => "at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa",
+    ],
+    'onepay' => [
+        "merchant_id" => "TESTONEPAY",
+        "access_code" => "6BEB2546",
+        "secure_secret" => "6D0870CDE5F24F34F3915FB0045120DB"
+    ],
+    'paypal' => [
+        'client_id' => 'AU__AjIAF-UDubzDsWsRVd24xpIOdnIrDVoKyO-SD1GF83eB1Fb__Ygv8-CnSYirYKeR25N4rO0IaKMY',
+        'client_secret' => 'EFFEbhdS1qavz5eDhvzVf1g3xI_hUYWj-SCOiySAYYS_BoL71F45_4cvi2hD89QezDdzPBNMvkGRtmDs',
+        'paypal_currency' => 'VND'
+    ]
+];
